40f77707d28bb890afe467b868916af2fc0372d8
Sergey "Shnatsel" Davidoff <shnatsel@gmail.com>
2024-05-12 04:27:02 +0100

    Flesh out the implementation but it produces incorrect results

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,73 @@
-use std::{ffi::OsString, process::Command};
+use std::{collections::BTreeMap, path::Path};
+
+use cargo_metadata::{MetadataCommand, PackageId};
+use tokei::Languages;
 
 fn main() {
-    let cargo = std::env::var_os("CARGO").unwrap_or("cargo".into());
-    let mut args: Vec<_> = std::env::args_os().skip(1).collect();
-    if Some(&OsString::from("loc")) == args.first() {
+    let mut args: Vec<_> = std::env::args().skip(1).collect();
+    if Some("loc") == args.first().map(|s| s.as_str()) {
         args.remove(0);
     }
-    let status = Command::new(cargo)
-        .arg("fetch")
-        .args(args)
-        .status()
-        .expect("failed to invoke `cargo fetch`");
-    if ! status.success() {
-        // `cargo fetch` has already printed its error message, no need to add anything
-        std::process::exit(status.code().unwrap_or(1));
+
+    // TODO: rewrite --target into --filter-platform, other similar stuff
+    let metadata = MetadataCommand::new()
+        .other_options(args) // forwards our own arguments for feature selection, --offline etc
+        .verbose(true) // forwards errors to stdout
+        .exec()
+        .unwrap();
+
+    let resolve = metadata.resolve.unwrap();
+    if !resolve.root.is_some() {
+        unimplemented!(
+            "Running in the workspace root is not yet supported. Please run on a specific crate."
+        )
     }
+
+    // TODO: filter out dev-dependencies? And optionally build-dependencies?
+    // I've written this once for `cargo auditable`,
+    // maybe I should uplift this into the `cargo_metadata` crate
+    // to share code and stop everyone from reimplementing it over and over
+
+    let mut reports: BTreeMap<PackageId, Languages> = BTreeMap::new();
+
+    for node in &resolve.nodes {
+        // TODO: technically quadratic, but does it matter?
+        // Could be optimized with a BTreeMap but probably won't show up on profile
+        let pkg = metadata.packages.iter().find(|p| p.id == node.id).unwrap();
+
+        // Chop off the "/Cargo.toml" from the end of the manifest path
+        let dir = pkg.manifest_path.as_std_path().parent().unwrap();
+
+        // TODO: parallelize this even more? This is embarrassingly parallel. But is it worth the complexity?
+        let report = count_loc(dir);
+        reports.insert(pkg.id.clone(), report);
+    }
+
+    // TODO: print N largest dependencies
+
+    let mut combined_report = Languages::new();
+    for (_pkg, mut report) in reports {
+        combined_report.append(&mut report);
+    }
+
+    // TODO: fancy pretty-printing like in tokei itself
+    for (lang, stats) in &combined_report {
+        println!("{lang}, {}", stats.lines());
+    }
+}
+
+fn count_loc(dir: &Path) -> tokei::Languages {
+    let included = &[dir];
+
+    // The `tests` dir is not excluded because we cannot exclude same-file unit tests.
+    // If we cannot exclude them, at least handle them consistently so we can document the behavior.
+    // TODO: verify this really does the intended thing
+    let excluded = &["benches", "examples"];
+
+    let mut config = tokei::Config::default();
+    config.sort = Some(tokei::Sort::Lines);
+
+    let mut languages = tokei::Languages::new();
+    languages.get_statistics(included, excluded, &config);
+    languages
 }
